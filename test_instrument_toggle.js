4d90219eb443fa522d23b8ca8beacc38ac6e1664
jaimejaramilloarias <jaimejaramilloarias@gmail.com>
2025-08-23 17:33:59 -0500

    Preserve instrument name accents

--- test_instrument_toggle.js
+++ test_instrument_toggle.js
@@ -3,14 +3,14 @@ const { getVisibleNotes, setInstrumentEnabled } = require('./script');
 
 const notes = [
   { instrument: 'Flauta', start: 0, end: 1, noteNumber: 60, color: '#fff', shape: 'oval', family: 'Maderas de timbre "redondo"' },
-  { instrument: 'Violin', start: 0, end: 1, noteNumber: 65, color: '#fff', shape: 'diamond', family: 'Cuerdas frotadas' },
+  { instrument: 'Violín', start: 0, end: 1, noteNumber: 65, color: '#fff', shape: 'diamond', family: 'Cuerdas frotadas' },
 ];
 
 setInstrumentEnabled('Flauta', false);
-setInstrumentEnabled('Violin', true);
+setInstrumentEnabled('Violín', true);
 
 const visible = getVisibleNotes(notes);
 assert.strictEqual(visible.length, 1);
-assert.strictEqual(visible[0].instrument, 'Violin');
+assert.strictEqual(visible[0].instrument, 'Violín');
 
 console.log('Pruebas de activación/desactivación de instrumentos completadas');
