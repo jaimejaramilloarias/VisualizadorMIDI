4601d4533eff54e7a7537a01292a4909fffe5e29
jaimejaramilloarias <jaimejaramilloarias@gmail.com>
2025-10-17 19:00:08 -0500

    Add arrow navigation controls and animation refresh

--- test_ui_integration.js
+++ test_ui_integration.js
@@ -6,7 +6,10 @@ const dom = new JSDOM(`<!DOCTYPE html><html><body>
 <button id="play-stop"></button>
 <button id="seek-forward"></button>
 <button id="seek-backward"></button>
+<button id="seek-forward-arrow"></button>
+<button id="seek-backward-arrow"></button>
 <button id="restart"></button>
+<button id="refresh-animation"></button>
 <button id="aspect-16-9"></button>
 <button id="aspect-9-16"></button>
 <button id="full-screen"></button>
@@ -19,6 +22,8 @@ let playing = false;
 let playCalls = 0;
 let stopCalls = 0;
 let forwardCalls = 0;
+let backwardCalls = 0;
+let refreshCalls = 0;
 let toggleCalls = 0;
 
 initializeUI({
@@ -34,8 +39,13 @@ initializeUI({
   onForward: () => {
     forwardCalls++;
   },
-  onBackward: () => {},
+  onBackward: () => {
+    backwardCalls++;
+  },
   onRestart: () => {},
+  onRefresh: () => {
+    refreshCalls++;
+  },
   onAspect169: () => {},
   onAspect916: () => {},
   onFullScreen: () => {},
@@ -47,11 +57,17 @@ initializeUI({
 document.getElementById('play-stop').click();
 document.getElementById('play-stop').click();
 document.getElementById('seek-forward').click();
+document.getElementById('seek-forward-arrow').click();
+document.getElementById('seek-backward').click();
+document.getElementById('seek-backward-arrow').click();
+document.getElementById('refresh-animation').click();
 document.getElementById('toggle-fps').click();
 
 assert.strictEqual(playCalls, 1);
 assert.strictEqual(stopCalls, 1);
-assert.strictEqual(forwardCalls, 1);
+assert.strictEqual(forwardCalls, 2);
+assert.strictEqual(backwardCalls, 2);
+assert.strictEqual(refreshCalls, 1);
 assert.strictEqual(toggleCalls, 1);
 
 console.log('Pruebas de integración de UI completadas');
